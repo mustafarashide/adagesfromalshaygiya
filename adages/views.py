e93c25666e5a56b21c21f0f1e28e58b319b3c56b
mustafarashide <melhassan@haverford.edu>
2025-06-26 15:29:14 -0400

    created adages app

---
+++ adages/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import response
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the adages index.")
