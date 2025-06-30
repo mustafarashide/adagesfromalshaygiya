8352b8744f21ddf1c79e7a9ce3c37f028ca1f727
mustafarashide <melhassan@haverford.edu>
2025-06-30 15:27:45 -0400

    created adage models

--- adages/models.py
+++ adages/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Adage(models.Model):
+    adage_id = models.IntegerField()
+    adage_text = models.CharField(max_length=200)
+    adage_meaning = models.CharField(max_length=200)
+    review_status = models.BooleanField(default=False)
+
 
