2104d87d86705e249e99ce9d2728c709ddf797d0
mustafarashide <melhassan@haverford.edu>
2025-06-30 17:03:29 -0400

    default value for id

--- adages/models.py
+++ adages/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Adage(models.Model):
-    adage_id = models.IntegerField()
+    adage_id = models.IntegerField(default=None)
     adage_text = models.CharField(max_length=200)
     adage_meaning = models.CharField(max_length=200)
     review_status = models.BooleanField(default=False)
