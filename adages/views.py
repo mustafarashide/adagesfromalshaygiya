ca08e39c01858f646bbcca2398c06e04bfeb34b7
mustafarashide <melhassan@haverford.edu>
2025-06-30 17:03:53 -0400

    will use command for importing

--- adages/views.py
+++ adages/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-from django.http import response
+from django.http import HttpResponse
+from .models import Adage
+import json
+
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the adages index.")
+def home_view(request):
+    return render(request,'base.html') 
+
+'''
+def import_data(request):
+    """
+    takes an adages json string and creates adage
+    """
+    if request.method == 'POST' and request.FILES['json_file']:
+        json_file = request.FILES['json_file']
+        data = json.load(json_file)
+        for item in data:
+            adage = Adage(
+                    adage_id = item['id'],
+                    adage_text = item['adage_text'],
+                    adage_meaning = item['adage_meaning'],
+                    review_status = False
+                    )
+            adage.save()
+'''
