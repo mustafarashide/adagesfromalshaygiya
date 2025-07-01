4e37368f451d0d7ad3ca5d16eaebd4371f2a9c66
mustafarashide <melhassan@haverford.edu>
2025-07-01 09:18:57 -0400

    load_adages works

--- adages/management/commands/load_adages.py
+++ adages/management/commands/load_adages.py
@@ -1,15 +1,16 @@
 import json
 from adages.models import Adage
-from django.core.management.base import BaseCommand
-import argparse
+from django.core.management.base import BaseCommand, CommandError
 
 class Command(BaseCommand):
     help = 'Loads adages from a json file into the database.'
-    def handle(self, *args,**kwargs):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--filename', dest='filename',type=str,help='Enter name of json file with adages')
-        args=parser.parse_arges()
-        with open(args.filename) as f:
+
+    def add_arguments(self,parser):
+        parser.add_argument('--filename',type=str,help='Enter name of json file with adages')
+
+    def handle(self, *args,**options):
+        filename=options['filename']
+        with open(filename) as f:
             adages = json.load(f)
         for adage in adages:
             adage_id = adage["id"]
@@ -19,11 +20,13 @@ class Command(BaseCommand):
 
             try:
                 # look for existing adage by name
-                adage_obj = Adage.objects.get(name=adage_text)
+                adage_obj = Adage.objects.get(adage_text=adage_text)
 
                 # update the description if the adage exits
-                adage_obj.adage_text = adage_text
+                adage_obj.adage_meaning= adage_meaning
                 adage_obj.adage_id = adage_id
+                adage_obj.review_status = review_status
+                adage_obj.save()
             except Adage.DoesNotExist:
                 # create a new adage if it does not exist
                 Adage.objects.create(adage_id=adage_id,adage_text=adage_text,adage_meaning=adage_meaning,review_status=review_status)
