fba1834c555f3ec61a26d8dd0efecde054777c91
mustafarashide <melhassan@haverford.edu>
2025-07-04 22:03:33 -0400

    home page shows random adages

--- adages/views.py
+++ adages/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Adage
 import json
+import random
 
 
 # Create your views here.
 
 def home_view(request):
-    return render(request,'base.html') 
+    number_of_adages = Adage.objects.count()
+    random_adage=Adage.objects.all()[random.randrange(0,number_of_adages)]
+    return render(request,'base.html',{"random_adage":random_adage}) 
 
 '''
 def import_data(request):
